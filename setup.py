120c2ea18b764b2f17802863edeac133dc3a113b
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-05-26 22:19:58 +0300

    Version updated.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='santa.templates',
-    version='0.0',
+    version='0.1',
     description="",
     long_description=open("README.rst").read(),
     # Get more strings from
